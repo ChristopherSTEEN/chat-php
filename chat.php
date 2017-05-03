535720cdbb0f6b3a34f30b66d32309ff7922c18d
ChristopherSTEEN <csteen59@gmail.com>
2017-05-03 09:43:50 +0200

    design on the chat page

--- chat.php
+++ chat.php
@@ -31,6 +31,20 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<title>MoonChat, the php chat!</title>
 		<style>
+			#chat {
+				border: 1px solid black;
+				box-shadow: 5px 5px 5px blue;
+				height: 80%;
+				margin-left: 30%;
+				margin-bottom: 15px;
+				oveflow: scroll;
+				padding: 5px;
+				width: 40%;
+			}
+			form {
+				width: 40%;
+				margin-left: 30%;
+			}
 			.userchat {
 				color: blue;
 			}
@@ -45,15 +59,17 @@
 		</style>
 	</head>
 	<body>
+		<div id="chat">
 		<?php
 			$history = $PDO->query("SELECT * FROM chathistory INNER JOIN users ON users.id = chathistory.id_user");
 			foreach ($history as $row){
 		?>
 		<div class="userchat"><?php echo $row->firstname . " " . $row-> lastname . " <span>(" . $row->username . ")</span> : ";?></div><div class="messchat"><?php echo $row->message; ?></div><br/>
 		<?php } ?>
+		</div>
 		<form action="chat.php" method="POST">
-			<input type="text" name="message" id="message" placeholder="Entrez votre message ici">
-			<input type="submit" name="submit" value="Envoyer">
+			<center><input type="text" name="message" id="message" placeholder="Entrez votre message ici">
+			<input type="submit" name="submit" value="Envoyer"></center>
 		</form>
 	</body>
 </html>
